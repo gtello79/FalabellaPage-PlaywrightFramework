0b57d5675b91205b5af8857ac3e23844b41b53d8
gtello79 <gonzalotello79@gmail.com>
2026-01-18 14:08:26 -0300

    Adding visibility validation in Wedding Page

--- src/pages/WeddingHomePage.ts
+++ src/pages/WeddingHomePage.ts
@@ -6,10 +6,18 @@ export class WeddingHomePage {
   readonly urlWeddingPage: string = 'https://novios.falabella.com/';
 
   readonly weddingTitle:string = 'Novios Falabella, Novios, Lista de regalo, Premio, Aportes, Luna de Miel';
-
+  readonly carruselIndicatorButton: string;
+  readonly totalPromos: number = 4;
+  readonly conveniosSection: Locator;
+  readonly conveniosTitle: Locator;
+  readonly celebrationSection: Locator;
 
   constructor(page: Page) {
     this.page = page;
+    this.carruselIndicatorButton = "carousel indicator ";
+    this.conveniosSection = page.locator('#section-convenios');
+    this.conveniosTitle = page.locator('h2', { hasText: 'Los mejores Convenios' });
+    this.celebrationSection = page.getByRole('button', { name: "carousel indicator 1" });
   }
 
   async navigateTo(): Promise<void> {
@@ -23,5 +31,33 @@ export class WeddingHomePage {
   async validateTitle(): Promise<void> {
     const actualTitle = await this.getTitle();
     await expect(actualTitle).toBe(this.weddingTitle);
+  }
+
+  async isCarruselIndicatorVisible(index: number): Promise<void> {
+    const buttonLocator = this.carruselIndicatorButton + index;
+    
+    const indicatorButton = this.page.getByRole('button', { name: buttonLocator });
+    await expect(indicatorButton).toBeVisible();
+    await expect(indicatorButton).toBeEnabled();
+  }
+
+  async validateConveniosSectionIsVisible(): Promise<void> {
+    await expect(this.conveniosSection).toBeVisible();
+  }
+
+  async validateConveniosTitleIsVisible(): Promise<void> {
+    await expect(this.conveniosTitle).toBeVisible();
+  }
+
+  async clickOnCelebrationSection(): Promise<void> {
+    await this.celebrationSection.click();
+  }
+
+  async validateCelebrationSectionIsVisible(): Promise<void> {
+    await expect(this.celebrationSection).toBeVisible();
+  }
+
+  async validateCelebrationSectionIsEnabled(): Promise<void> {
+    await expect(this.celebrationSection).toBeEnabled();
   }
 }
--- tests/SmokeTest/WeddingPage/WeddingHomePage.spec.ts
+++ tests/SmokeTest/WeddingPage/WeddingHomePage.spec.ts
@@ -3,15 +3,34 @@ import { WeddingHomePage } from '../../../src/pages/WeddingHomePage';
 
 test.describe('Validate Falabella Wedding Page ', () => {
     let weddingPage: WeddingHomePage;
-    
+
     test.beforeEach(async ({ page }) => {
         weddingPage = new WeddingHomePage(page);
         await weddingPage.navigateTo();
     });
 
-    test('Verify title in Wedding Page', async ({ page }) => {
+    test('Verify title in Wedding Page', async ({ }) => {
         // Navigate to Home Page
-        let titleWedding = await page.title();
         await weddingPage.validateTitle();
+    });
+
+    test('Verify Carrusel promotions is visible', async ({ }) => {
+        for (let i = 1; i <= weddingPage.totalPromos; i++) {
+            await weddingPage.isCarruselIndicatorVisible(i);
+        }
+    });
+
+    test('Verify Convenios section is available', async ({ }) => {
+        await weddingPage.validateConveniosSectionIsVisible();
+    });
+
+    test('Verify that Convenios title is visible and contains the correct text', async ({ }) => {
+        await weddingPage.validateConveniosTitleIsVisible();
+    });
+
+    test('Verify presence of Wedding Gift List link', async ({ }) => {
+        await weddingPage.clickOnCelebrationSection();
+        await weddingPage.validateCelebrationSectionIsVisible();
+        await weddingPage.validateCelebrationSectionIsEnabled();
     });
 });
